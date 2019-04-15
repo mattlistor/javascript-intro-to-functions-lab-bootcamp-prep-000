fe65627eb161b4e46e12df0df08bccd03bb0f029
Matthew Listor <mattlistor@gmail.com>
2019-04-15 20:08:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
   else if (string.toUpperCase() === string) {
-    return "I can't hear you!"
+    return "YES INDEED!"
   }    
+  else if ()
 }
